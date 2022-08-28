f9c7b508311e8dc52c3c97147d066a3a63e406a3
Kimia Takht sangi <56971697+kimiatakhtsangy@users.noreply.github.com>
2022-08-28 21:39:02 +0430

    add navbar

---
+++ dashboard-test/src/Components/Header/navbar.jsx
@@ -0,0 +1,14 @@
+import HomeIcon from "@mui/material"
+function navbar() {
+    return(
+        <div>
+            <HomeIcon color="disabled" />
+            <p>/ Dashboard</p>
+            <h2>Dashboard</h2>
+
+
+        </div>
+
+    );
+}
+export default navbar;
--- dashboard-test/src/index.js
+++ dashboard-test/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+// import App from './App';
+import navbar from './Components/Header/navbar';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <navbar />
+    {/* <App /> */}
   </React.StrictMode>
 );
 
